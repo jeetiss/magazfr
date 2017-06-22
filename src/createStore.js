9d280ea711740a52d8d2c03a63cb3dfd630876da
jeetiss <za5rubley@gmail.com>
2017-06-22 13:50:20 +0000

    rebuild pages at 91ff6f5

--- src/createStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore as cs, combineReducers, applyMiddleware } from 'redux'
-import { pages, activePage, bucket } from './reducers/reducer'
-import thunk from 'redux-thunk'
-
-export default function createStore () {
-  return cs(combineReducers({ pages, activePage, bucket }), applyMiddleware(thunk))
-}
